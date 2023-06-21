145214aaa571bc59d450aaab284f4f7324d7af21
Daniil3200 <dania.vailo@mail.ru>
2023-06-21 20:38:12 +0300

    Скрипт Bash №2

---
+++ script2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Peremena1=92 
+Peremena2=21
+echo "Добавление переменных Peremena1 и Peremena2 со значением $Peremena1 и $Peremena2:"
+
+echo "Добавление переменны Symm для решение примера с переменнами и вывод их в консоль:"
+Symm=$(( $Peremena1 + $Peremena2 ))
+
+echo "Сумма $Peremena1 и $Peremena2 равна $Symm" 
