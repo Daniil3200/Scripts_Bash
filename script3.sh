b86fc14a79ef7b4c43f5d3d50baebd39ad987f13
Daniil3200 <dania.vailo@mail.ru>
2023-07-05 14:12:09 +0300

    Третий скрипт

--- script3.sh
+++ script3.sh
@@ -3,28 +3,19 @@
 input=""
 while [ "$input" != "exit" ]
 do
-    echo -n "script3($mode) > " && read input
-    if [ "$input" = "config" ]
+    echo -n "script3($move) > " && read input
+    if [ "$input" = "install" ]
     then
-        mode="config"
-    elif [ "$input" = "up" ]
+        sudo apt install tilda
+        echo "CLI Tilda установлена"
+    elif [ "$input" = "delete" ]
     then
-        mode=""
+        sudo apt-get remove tilda -y
+        echo "CLI Tilda удалена"
+
     elif [ "$input" = "exit" ]
     then
-    break
-    else
-        if [ "$mode" = "config" ]
-        then
-            if [ "$input" = "list" ]
-            then
-                echo "ABCD"
-            else
-                echo "Invalid"
-            fi
-        else
-            echo "Invalid"
-        fi
+    break 
     fi
 done
 
